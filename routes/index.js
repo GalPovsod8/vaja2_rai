64f0da01fb809097d04b83d725a0d3e31e8405ca
GalPovsod8 <galpos10@gmail.com>
2025-03-30 00:09:45 +0100

    added a bunch of changes :)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "StackUnderflow | Less answers, more confusion!",
+    section1Title: "Welcome to StackUnderflow",
+    section1Content:
+      "StackUnderflow is a Q&A platform where you can ask questions and get answers from the community. However, be prepared for confusion and less clarity!",
+  });
 });
 
 module.exports = router;
